f02283a4b93e93b32757f5392185519d977dbd85
Lucas Gomes <lucasgsm88@gmail.com>
2018-10-01 10:55:47 -0300

    removed connection infos

---
+++ dist/enums/heroclass.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var HeroClass;
+(function (HeroClass) {
+    HeroClass["MAGE"] = "Mage";
+    HeroClass["WARRIOR"] = "Warrior";
+    HeroClass["HUNTER"] = "Hunter";
+    HeroClass["THIEF"] = "Thief";
+})(HeroClass = exports.HeroClass || (exports.HeroClass = {}));
+//# sourceMappingURL=heroclass.js.map

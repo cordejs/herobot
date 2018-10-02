484d6f89e15b12e97a77f01d739432ea070d01c7
lgmagalhaes <lucasgsm88@gmail.com>
2018-10-02 18:11:01 -0300

    Created connection template

---
+++ connection_template.ts
@@ -0,0 +1,12 @@
+export const firebaseConnection = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
+};
+
+export const SuperSecretDiscordToken = {
+    token: "PUT HERE YOUR DISCORD TOKEN"
+};

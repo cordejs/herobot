389e2e71f3ffef1e075f513987bbeac05a41000e
Lucas Gomes <lucasgsm88@gmail.com>
2018-10-03 13:36:38 -0300

    removed dist folder

--- dist/src/enums/proficienceType.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ProficienceType;
-(function (ProficienceType) {
-    ProficienceType["DAMAGE"] = "Damage";
-    ProficienceType["SHIELD"] = "Shield";
-})(ProficienceType = exports.ProficienceType || (exports.ProficienceType = {}));
-//# sourceMappingURL=proficienceType.js.map

92b823ee6c97aa4211d7de800b0a7278e5e9db3e
hkirschke <33073460+hkirschke@users.noreply.github.com>
2018-10-16 23:26:34 -0300

    Update src/commands/shop.ts
    Update shop.ts

--- src/commands/shop.ts
+++ src/commands/shop.ts
@@ -1,25 +1,42 @@
-import * as Discord from "discord.js";
-import { playerService } from "../lib/services/playerService";
-
+import * as Discord from "discord.js"; 
+import { playerService } from "../lib/services/playerService";  
+import { getItemType } from "../lib/utils/itemTypeHandle"; 
+import { BaseEntityService } from "../lib/services/baseEntityService";
 /**
- * Inform player's experience
+ * Informs all available items from selected type.
  * @param msg Discord last message related to the command
  */
-export function xp(msg: Discord.Message) {
-  playerService.findbyUserID(msg.author.id).then(player => {
-    if (player !== null) {
-      const percentage = (player.xp * 100) / player.levelMaxXp;
-      msg.channel.send(
-        "You are current in level " +
-          player.level +
-          ". Your experience is " +
-          player.xp +
-          "/" +
-          player.levelMaxXp +
-          " (" +
-          percentage +
-          "%)"
-      );
+
+export function shop(msg: Discord.Message) { 
+  // First check if the user already have an player 
+  msg.channel.send("Want to buy sword or shield ?").then(() => {
+    msg.channel
+    .awaitMessages(
+      responseName => responseName.author.id === msg.author.id,
+      {
+        max: 1,
+        time: 10000,
+        errors: ["time"]
+      }
+    ).then(getItemType => {
+        const itemTypeName = getItemType.first().content;
+         var bdservice = new BaseEntityService();  
+         readTextFile("../data/" + itemTypeName + ".json", function(text){
+            var data = JSON.parse(text);
+            msg.channel.send(data);
+         }); 
+    });
+  }); 
+
+  function readTextFile(file, callback) {
+    var jsonFile = new XMLHttpRequest();
+    jsonFile.overrideMimeType("application/json");
+    jsonFile.open("GET", file, true);
+    jsonFile.onreadystatechange = function() {
+        if (jsonFile.readyState === 4 && jsonFile.status == 200) {
+            callback(jsonFile.responseText);
+        }
     }
-  });
+    jsonFile.send(null);
+   }
 }

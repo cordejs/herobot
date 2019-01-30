0e2b0be7d6274f71d6547dc30948eb7179e41c4c
Lucas Gomes <lucasgsm88@gmail.com>
2019-01-30 14:50:17 -0200

    returned data generation script

---
+++ data/creation/generate.js
@@ -0,0 +1,43 @@
+/**
+ * creates a JSON with all proficience levels
+ */
+generateProficienceJSON = function () {
+    let json = "{\n";
+    const repets = 200;
+
+    for (var i = 1; i <= repets; i++) {
+        json += `"${i}": {\n`;
+        json += `  "exp": ${(i + 1) * (100 + i + 1) + Math.pow(i, 2)} \n`;
+        json += "}";
+
+        if (i < repets) {
+            json += ",\n";
+        }
+    }
+    json += "\n";
+    json += "}";
+
+    console.log(json);
+};
+
+/**
+ * creates a JSON with all player levels
+ */
+generateLevelsJSON = function () {
+    let json = "{\n";
+    const repets = 200;
+
+    for (var i = 1; i <= repets; i++) {
+        json += `"${i}": {\n`;
+        json += `  "exp": ${i * (100 + i) + Math.pow(i, 2)} \n`;
+        json += "}";
+
+        if (i < repets) {
+            json += ",\n";
+        }
+    }
+    json += "\n";
+    json += "}";
+
+    console.log(json);
+};

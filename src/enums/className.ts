937d9325ee60737945fe518af5340749d31642fe
Lucas Gomes <lucasgsm88@gmail.com>
2019-02-17 19:08:05 -0300

    created Classes

--- src/enums/className.ts
+++ src/enums/className.ts
@@ -1,6 +1,47 @@
+/**
+ * 1º evolution of class after begginer
+ */
 export const enum ClassName {
   MAGE = "Mage",
   WARRIOR = "Warrior",
-  HUNTER = "Hunter",
+  ARCHER = "Archer",
   THIEF = "Thief"
+}
+
+/**
+ * 2º evolution of class
+ */
+export const enum ClassNameEvolve {
+  /**
+   * Evolution of Warrior
+   */
+  KNIGHT = "Kinight",
+  /**
+   * Evolution of Warrior
+   */
+  PALADIN = "Paladin",
+  /**
+   * Evolution of Mage
+   */
+  SORCERER = "Sorcerer",
+  /**
+   * Evolution of Mage
+   */
+  NECRO = "Necro",
+  /**
+   * Evolution of Thief
+   */
+  LADIN = "Ladin",
+  /**
+   * Evolution of Thief
+   */
+  NINJA = "Ninja",
+  /**
+   * Evolution of Archer
+   */
+  HUNTER = "Hunter",
+  /**
+   * Evolution of Archer
+   */
+  TRICKSTER = "Trickster"
 }

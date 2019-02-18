93eed83b39e91278977b9199756da4d96ff81fdc
Lucas Gomes <lucasgsm88@gmail.com>
2019-02-18 13:28:30 -0300

    finished classes insert (not tested)

--- src/interfaces/heroClass.ts
+++ src/interfaces/heroClass.ts
@@ -125,7 +125,7 @@ export const Hunter: IHeroClass = {
 };
 
 export const Trickster: IHeroClass = {
-  name: ClassNameEvolve.HUNTER,
+  name: ClassNameEvolve.TRICKSTER,
   attackspeedbuff: 1,
   damagebuff: 5,
   expbuff: 0,
--- src/migration/1550502138205-migration.ts
+++ src/migration/1550502138205-migration.ts
@@ -1,27 +1,59 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
-import { ClassName } from "../enums/className";
+import { ClassName, ClassNameEvolve } from "../enums/className";
 
 export class Migration1550502138205 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
     queryRunner.query(
-      `INSERT INTO heroclass (name, attackspeedbuff, damagebuff, expbuff, goldbuff, hpbuff, shieldbuff) SELECT ${
-        ClassName.WARRIOR
-      }, 0, 0, 0, 0, 5, 0`
+      `INSERT INTO heroclass (name, attackspeedbuff, damagebuff, expbuff, goldbuff, hpbuff, shieldbuff) ` +
+        +`SELECT ${ClassName.WARRIOR}, 0, 0, 0, 0, 5, 0`
+    );
+
+    queryRunner.query(
+      `INSERT INTO heroclass (name, attackspeedbuff, damagebuff, expbuff, goldbuff, hpbuff, shieldbuff) ` +
+        +`SELECT ${ClassName.MAGE}, 0, 0, 3, 0, 0, 0`
     );
     queryRunner.query(
-      `INSERT INTO heroclass (name, attackspeedbuff, damagebuff, expbuff, goldbuff, hpbuff, shieldbuff) SELECT ${
-        ClassName.MAGE
-      }, 0, 0, 3, 0, 0, 0`
+      `INSERT INTO heroclass (name, attackspeedbuff, damagebuff, expbuff, goldbuff, hpbuff, shieldbuff)` +
+        +` SELECT ${ClassName.THIEF}, 0, 0, 0, 1, 0, 0`
     );
     queryRunner.query(
-      `INSERT INTO heroclass (name, attackspeedbuff, damagebuff, expbuff, goldbuff, hpbuff, shieldbuff) SELECT ${
-        ClassName.THIEF
-      }, 0, 0, 0, 1, 0, 0`
+      `INSERT INTO heroclass (name, attackspeedbuff, damagebuff, expbuff, goldbuff, hpbuff, shieldbuff) ` +
+        +`SELECT ${ClassName.ARCHER}, 0, 0, 0, 0, 5, 0`
     );
+
     queryRunner.query(
-      `INSERT INTO heroclass (name, attackspeedbuff, damagebuff, expbuff, goldbuff, hpbuff, shieldbuff) SELECT ${
-        ClassName.ARCHER
-      }, 0, 0, 0, 0, 5, 0`
+      `INSERT INTO heroclass (name, attackspeedbuff, damagebuff, expbuff, goldbuff, hpbuff, shieldbuff)` +
+        `SELECT ${ClassNameEvolve.KNIGHT}, 0, 0, 0, 1, 10, 0`
+    );
+
+    queryRunner.query(
+      `INSERT INTO heroclass (name, attackspeedbuff, damagebuff, expbuff, goldbuff, hpbuff, shieldbuff) ` +
+        +`SELECT ${ClassNameEvolve.PALADIN}, 0, 5, 0, 1, 5, 0`
+    );
+
+    queryRunner.query(
+      `INSERT INTO heroclass (name, attackspeedbuff, damagebuff, expbuff, goldbuff, hpbuff, shieldbuff) ` +
+        +` SELECT ${ClassNameEvolve.NECRO}, 0, 0, 0, 0, 5, 0`
+    );
+
+    queryRunner.query(
+      `INSERT INTO heroclass (name, attackspeedbuff, damagebuff, expbuff, goldbuff, hpbuff, shieldbuff) ` +
+        +`SELECT ${ClassNameEvolve.LADIN}, 0, 0, 0, 1, 5, 0`
+    );
+
+    queryRunner.query(
+      `INSERT INTO heroclass (name, attackspeedbuff, damagebuff, expbuff, goldbuff, hpbuff, shieldbuff) ` +
+        +`SELECT ${ClassNameEvolve.NINJA}, 0, 5, 0, 1, 0, 0`
+    );
+
+    queryRunner.query(
+      `INSERT INTO heroclass (name, attackspeedbuff, damagebuff, expbuff, goldbuff, hpbuff, shieldbuff) ` +
+        +` SELECT ${ClassNameEvolve.HUNTER}, 1, 10, 0, 1, 0, 0`
+    );
+
+    queryRunner.query(
+      `INSERT INTO heroclass (name, attackspeedbuff, damagebuff, expbuff, goldbuff, hpbuff, shieldbuff) ` +
+        +`SELECT ${ClassNameEvolve.TRICKSTER}, 1, 5, 0, 1, 5, 0`
     );
   }
 

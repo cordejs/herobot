8ddd02064d08b66cbf92796248672132b819e2f0
Lucas Gomes <lucasgsm88@gmail.com>
2019-02-18 22:07:56 -0300

    simple data generation

--- data/creation/generate.js
+++ data/creation/generate.js
@@ -1,9 +1,17 @@
+function generate(reps = 200, type) {
+  if (type === "proficience") {
+    generateProficienceJSON(reps);
+  } else {
+    generateLevelsJSON(reps);
+  }
+}
+
 /**
  * creates a JSON with all proficience levels
  */
-generateProficienceJSON = function() {
+function generateProficienceJSON(reps = 200) {
   let json = "{\n";
-  const repets = 200;
+  const repets = reps;
 
   for (var i = 1; i <= repets; i++) {
     json += `"${i}": {\n`;
@@ -17,20 +25,20 @@ generateProficienceJSON = function() {
   json += "\n";
   json += "}";
 
-  console.log(json);
-};
+  document.getElementById("txtAreaResult").value = json;
+}
 
 /**
  * creates a JSON with all player levels
  */
-generateLevelsJSON = function() {
+function generateLevelsJSON(reps = 200) {
   let json = "{\n";
-  const repets = 200;
+  const repets = reps;
 
   for (var i = 1; i <= repets; i++) {
-    json += `"${i}": {\n`;
-    json += `  "exp": ${i * (100 + i) + Math.pow(i, 2)} \n`;
-    json += "}";
+    json += ` "${i}": {\n`;
+    json += `   "exp": ${i * (100 + i) + Math.pow(i, 2)} \n`;
+    json += " }";
 
     if (i < repets) {
       json += ",\n";
@@ -39,5 +47,5 @@ generateLevelsJSON = function() {
   json += "\n";
   json += "}";
 
-  console.log(json);
-};
+  document.getElementById("txtAreaResult").value = json;
+}

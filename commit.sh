bf1aa2e012b6ce8458e8ad25afd8aed37a24fd7c
Lucas Gomes Da Silva Magalhães <1047825@sga.pucminas.br>
2019-02-21 19:44:49 -0300

    fixed commit script

--- commit.sh
+++ commit.sh
@@ -1,8 +1,20 @@
 #!/bin/bash -e
+
 commit_message="$1"
+
 if [ -z "$commit_message"]
 then 
-    commit_message="Default message"
+
+    echo -e "\e[31mNo commit message was informed. Please describe something"
+    echo -ne -e "\e[39mMessage: "
+    read commit_message
+    
+    while [[ -z "$commit_message" ]]
+     do
+        echo "There still no message."
+        echo -ne "Message: "
+        read commit_message
+    done     
 fi
 
 git add .

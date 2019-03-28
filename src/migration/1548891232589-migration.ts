66eea0bb9fbdbbcf5d8d60cc079d7f85d0ede704
Lucas Gomes <lucasgsm88@gmail.com>
2019-03-27 22:58:31 -0300

    removing trash

--- src/migration/1548891232589-migration.ts
+++ src/migration/1548891232589-migration.ts
@@ -2,73 +2,116 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class Migration1548891232589 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query("DROP TABLE IF EXISTS heroclass CASCADE;");
+    await queryRunner.query("DROP TABLE IF EXISTS equip CASCADE;");
+    await queryRunner.query("DROP TABLE IF EXISTS adventure CASCADE;");
+    await queryRunner.query("DROP TABLE IF EXISTS monster CASCADE;");
+    await queryRunner.query("DROP TABLE IF EXISTS playstatus CASCADE;");
+    await queryRunner.query("DROP TABLE IF EXISTS inventory_item CASCADE;");
+    await queryRunner.query("DROP TABLE IF EXISTS hero CASCADE;");
+    await queryRunner.query("DROP TABLE IF EXISTS weapon CASCADE;");
+    await queryRunner.query("DROP TABLE IF EXISTS shield CASCADE;");
+    await queryRunner.query("DROP TABLE IF EXISTS item CASCADE;");
+    await queryRunner.query("DROP TABLE IF EXISTS proficience CASCADE;");
+    await queryRunner.query("DROP TABLE IF EXISTS play_status CASCADE;");
+
     await queryRunner.query(
-      "DROP TABLE IF EXISTS adventure CASCADE; " +
-        "DROP TABLE IF EXISTS monster CASCADE;" +
-        "DROP TABLE IF EXISTS playstatus CASCADE;" +
-        "DROP TABLE IF EXISTS inventory_item CASCADE;" +
-        "DROP TABLE IF EXISTS hero CASCADE;" +
-        "DROP TABLE IF EXISTS weapon CASCADE;" +
-        "DROP TABLE IF EXISTS shield CASCADE;" +
-        "DROP TABLE IF EXISTS item CASCADE;" +
-        "DROP TABLE IF EXISTS proficience CASCADE;" +
-        "DROP TABLE IF EXISTS play_status CASCADE;" +
-        "CREATE TABLE proficience (" +
+      "CREATE TABLE heroclass (" +
+        "    id SERIAL," +
+        "    name CHARACTER(30) NOT NULL," +
+        "    damageBuff INTEGER NOT NULL," +
+        "    shieldBuff INTEGER NOT NULL," +
+        "    hpBuff INTEGER NOT NULL," +
+        "    goldBuff INTEGER NOT NULL," +
+        "    expBuff INTEGER NOT NULL," +
+        "    attackSpeedBuff INTEGER NOT NULL," +
+        "    PRIMARY KEY(id)" +
+        ");"
+    );
+
+    await queryRunner.query(
+      "CREATE TABLE equip(" +
+        "id SERIAL PRIMARY KEY," +
+        "name CHARACTER(30) NOT NULL," +
+        "level INTEGER NOT NULL," +
+        "price INTEGER NOT NULL)"
+    );
+
+    await queryRunner.query(
+      "CREATE TABLE proficience (" +
         "   id SERIAL PRIMARY KEY," +
         "    level INTEGER NOT NULL," +
         "    xp INTEGER NOT NULL," +
         "    levelMaxXp INTEGER NOT NULL" +
-        ");" +
-        "CREATE TABLE monster (" +
+        ");"
+    );
+
+    await queryRunner.query(
+      "CREATE TABLE monster (" +
         "    id SERIAL PRIMARY KEY," +
         "    name CHARACTER(30) NOT NULL," +
         "    level INTEGER NOT NULL," +
         "    damage INTEGER NOT NULL," +
         "    hp INTEGER NOT NULL," +
-        "    shield INTEGER NOT NULL," +
+        "    defence INTEGER NOT NULL," +
         "    givedxp INTEGER NOT NULL," +
-        "    givedgold INTEGER NOT NULL" +
-        ");" +
-        "CREATE TABLE item (" +
+        "    givedgold INTEGER NOT NULL," +
+        "    equipDropChance INTEGER NOT NULL," +
+        "    idEquipDrop SERIAL," +
+        "    FOREIGN KEY (idEquipDrop) REFERENCES Equip(id)" +
+        ");"
+    );
+
+    await queryRunner.query(
+      "CREATE TABLE item (" +
         "    id SERIAL PRIMARY KEY," +
         "    name CHARACTER(50) NOT NULL," +
         "    price INTEGER NOT NULL" +
-        ");" +
-        "CREATE TABLE shield (" +
-        "    id SERIAL PRIMARY KEY," +
+        ");"
+    );
+
+    await queryRunner.query(
+      "CREATE TABLE shield (" +
+        "    id SERIAL REFERENCES equip(id)," +
         "    defence INTEGER NOT NULL," +
-        "    idItem SERIAL," +
-        "    FOREIGN KEY(idItem) REFERENCES item(id)" +
-        ");" +
-        "CREATE TABLE weapon (" +
-        "    id SERIAL PRIMARY KEY," +
+        "    PRIMARY KEY(id)" +
+        ");"
+    );
+
+    await queryRunner.query(
+      "CREATE TABLE weapon (" +
+        "    id SERIAL REFERENCES equip(id)," +
         "    damage INTEGER NOT NULL," +
-        "    idItem SERIAL," +
-        "    FOREIGN KEY(idItem) REFERENCES item(id)" +
-        ");" +
-        "CREATE TABLE adventure (" +
+        "    PRIMARY KEY(id)" +
+        ");"
+    );
+
+    await queryRunner.query(
+      "CREATE TABLE adventure (" +
         "    id SERIAL PRIMARY KEY," +
         "    level INTEGER NOT NULL," +
         "    name CHARACTER(40) NOT NULL," +
-        "    idMonster SERIAL NOT NULL," +
+        "    idMonster INTEGER NOT NULL," +
         "    FOREIGN KEY(idMonster) REFERENCES monster(id)" +
-        ");" +
-        "CREATE TABLE play_status (" +
+        ");"
+    );
+
+    await queryRunner.query(
+      "CREATE TABLE play_status (" +
         "    id SERIAL PRIMARY KEY," +
-        "    action CHARACTER(30) NOT NULL," +
-        "    monsterskilled INTEGER NOT NULL," +
-        "    exp INTEGER NOT NULL," +
-        "    timeStarted INTEGER NOT NULL" +
-        ");" +
-        "CREATE TABLE inventory_item (" +
-        "    id SERIAL PRIMARY KEY," +
-        "    amount INTEGER NOT NULL," +
-        "    equiped BIT(1) NOT NULL," +
-        "    idItem SERIAL," +
-        "    FOREIGN KEY(idItem) REFERENCES item(id)" +
-        ");" +
-        "CREATE TABLE hero (" +
-        "    id SERIAL PRIMARY KEY," +
+        "    task CHARACTER(30) NOT NULL," +
+        "    gold INTEGER NOT NULL," +
+        "    monsterskilled INTEGER NOT NULL DEFAULT 0," +
+        "    exp INTEGER NOT NULL DEFAULT 0," +
+        "    timeStarted INTEGER NOT NULL DEFAULT 0," +
+        "    idadventure SERIAL," +
+        "    FOREIGN KEY (idadventure) REFERENCES adventure(id)" +
+        ");"
+    );
+
+    await queryRunner.query(
+      "CREATE TABLE hero (" +
+        "    id BIGINT PRIMARY KEY," +
         "    name CHARACTER(30) NOT NULL," +
         "    level INTEGER NOT NULL," +
         "    hpTotal INTEGER NOT NULL," +
@@ -79,16 +122,30 @@ export class Migration1548891232589 implements MigrationInterface {
         "    deaths INTEGER NOT NULL," +
         "    monstersKilled INTEGER NOT NULL," +
         "    heroClass CHARACTER(30) NOT NULL," +
-        "    idShield SERIAL," +
-        "    idWeapon SERIAL," +
-        "    idPlayStatus SERIAL," +
-        "    idDamageProficience SERIAL," +
-        "    idDefenceProficience SERIAL," +
+        "    idShield INTEGER," +
+        "    idWeapon INTEGER," +
+        "    idPlayStatus INTEGER," +
+        "    idDamageProficience INTEGER," +
+        "    idDefenceProficience INTEGER," +
+        "    idHeroClass INTEGER," +
         "    FOREIGN KEY(idShield) REFERENCES shield(id)," +
         "    FOREIGN KEY(idWeapon) REFERENCES weapon(id)," +
         "    FOREIGN KEY(idPlayStatus) REFERENCES play_status(id)," +
         "    FOREIGN KEY(idDamageProficience) REFERENCES proficience(id)," +
-        "    FOREIGN KEY(idDefenceProficience) REFERENCES proficience(id)" +
+        "    FOREIGN KEY(idDefenceProficience) REFERENCES proficience(id)," +
+        "    FOREIGN KEY(idHeroClass) REFERENCES heroclass(id)" +
+        ");"
+    );
+
+    await queryRunner.query(
+      "CREATE TABLE inventory_item (" +
+        "    id SERIAL PRIMARY KEY," +
+        "    idhero BIGINT," +
+        "    amount INTEGER NOT NULL," +
+        "    equiped BIT(1) NOT NULL," +
+        "    idItem INTEGER," +
+        "    FOREIGN KEY(idItem) REFERENCES item(id)," +
+        "    FOREIGN KEY(idhero) REFERENCES hero(id)" +
         ");"
     );
   }
--- src/migration/1550432009741-migration.ts
+++
@@ -1,9 +0,0 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
-
-export class Migration1550432009741 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<any> {
-    queryRunner.query("ALTER TABLE hero ALTER COLUMN id TYPE bigint");
-  }
-
-  public async down(queryRunner: QueryRunner): Promise<any> {}
-}
